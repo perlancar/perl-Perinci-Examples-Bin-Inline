f7292ec940573b53ad41cd008c13bda53d72d876
perlancar (@pc-home) <perlancar@gmail.com>
2016-04-19 17:43:22 +0700

    Add scripts: peri-eg-check-schema-{simple,date}-inline

--- lib/Perinci/Examples/Bin/Inline.pm
+++ lib/Perinci/Examples/Bin/Inline.pm
@@ -9,8 +9,46 @@ $SPEC{noop} = {
     v => 1.1,
     summary => 'Do nothing',
 };
+sub noop { [200, "OK", ""] }
 
-sub noop { [200] }
+$SPEC{check_schema_simple} = {
+    v => 1.1,
+    summary => 'Test schema checking (simple types: int, str)',
+    args => {
+        arg1 => {
+            req => 1,
+            pos => 0,
+            schema => ['int*', min=>0],
+        },
+        arg2 => {
+            schema => ['str*', {
+                min_len => 2,
+                max_len => 8,
+                match => '\A(?=[aeiou])(?=[^aeiou])',
+            }],
+        },
+    },
+};
+sub check_schema_simple {
+    my %args = @_;
+    [200, "OK", \%args];
+}
+
+$SPEC{check_schema_date} = {
+    v => 1.1,
+    summary => 'Test schema checking (date type)',
+    args => {
+        arg1 => {
+            req => 1,
+            pos => 0,
+            schema => ['date*'],
+        },
+    },
+};
+sub check_schema_date {
+    my %args = @_;
+    [200, "OK", \%args];
+}
 
 1;
 # ABSTRACT: Sample Perinci::CmdLine::Inline-generated CLI scripts
